bf6216bb70bbbb58b3c0dc2365ccfef16b886122
Zensey <jnashicq@gmail.com>
2018-05-19 19:20:14 +0300

    enhances

--- cmd/demo/handler.go
+++ cmd/demo/handler.go
@@ -7,22 +7,86 @@ import (
 	_ "image/gif"
 	_ "image/png"
 	"image"
-	"github.com/nfnt/resize"
 	"strings"
 	"bytes"
 	"image/png"
 	"encoding/base64"
 	"encoding/json"
+	"io"
+	"runtime/debug"
+	"github.com/nfnt/resize"
 )
 
-type TResponse []string
-type TRequest  []string
-type TRequest_ struct {
-	Urls []string `json:"urls,omitempty"`
+type TResponse struct {
+   Thumbs []string  `json:"thumbs,omitempty"`
+   thumbnails *[]image.Image
+}
+
+func NewTResponse() TResponse {
+	return TResponse{thumbnails: &[]image.Image{}}
+}
+
+func (r TResponse) addThumb(im image.Image) {
+	*r.thumbnails = append(*r.thumbnails, resize.Resize(thumbDim, thumbDim, im, resize.NearestNeighbor))
+}
+
+func (r TResponse) writeResp(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	for _, v := range *r.thumbnails {
+		//s.l.Info("s> t")
+		var buff bytes.Buffer
+		png.Encode(&buff, v)
+		encoded := base64.StdEncoding.EncodeToString(buff.Bytes())
+
+		r.Thumbs = append(r.Thumbs, encoded)
+	}
+	encoder := json.NewEncoder(w)
+	encoder.Encode(r)
+}
+
+type TRequest struct {
+	Urls []string `json:"imgsUrls,omitempty"`
 	Imgs []string `json:"imgs,omitempty"`
 }
 
-const thumbDim = 1
+func ReadTRequest(r io.Reader) (TRequest, error){
+	obj := TRequest{}
+	err := json.NewDecoder(r).Decode(&obj)
+	return obj, err
+}
+
+func (r *TRequest) handleImgs(s *HandlerCtx, res TResponse) error {
+	for _, v := range r.Imgs {
+		input := base64.NewDecoder(base64.StdEncoding, strings.NewReader(v))
+		im, imageType, err := image.Decode(input)
+		if err != nil {
+			s.l.Info("s> image.Decode", err)
+		}
+		s.l.Info("s> i got", imageType)
+		res.addThumb(im)
+	}
+	return nil
+}
+
+func (r *TRequest) handleUrls(s *HandlerCtx, res TResponse) error {
+	for _,u := range r.Urls {
+		//s.l.Info("s> url", u)
+		resp, err := http.Get(u)
+		if err != nil {
+			s.l.Info("s> get", err)
+			continue
+		}
+		im, _, err := image.Decode(resp.Body)
+		if err != nil {
+			s.l.Info("s> image.Decode", err)
+			continue
+		}
+		res.addThumb(im)
+	}
+	return nil
+}
+
+const thumbDim = 10
 
 type HandlerCtx struct {
 	mux    *http.ServeMux
@@ -32,28 +96,30 @@ type HandlerCtx struct {
 func NewHandler(log logger.Logger) *HandlerCtx {
 	s := &HandlerCtx{mux: http.NewServeMux()}
 	s.l = log
-
 	s.mux.HandleFunc("/upload", s.index)
 	return s
 }
 
 func (s *HandlerCtx) index(w http.ResponseWriter, r *http.Request) {
 	s.l.Info(r.RequestURI)
+	defer func() {
+		if r := recover(); r != nil {
+			s.l.Infof("%s: %s", r, debug.Stack()) // line 20
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500"))
+		}
+	}()
 
 	hh, ok:= r.Header["Content-Type"]
 	if ok && strings.HasPrefix(hh[0], "multipart/form-data;") {
+		resp := NewTResponse()
 		reader, err := r.MultipartReader()
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("500"))
-			return
+			panic(err)
 		}
-
-		var thumbnails []image.Image
 		for {
 			nr, err := reader.NextPart()
 			if err != nil {
-				s.l.Info("s>", err)
 				break
 			}
 			contentType := nr.Header["Content-Type"][0]
@@ -64,79 +130,29 @@ func (s *HandlerCtx) index(w http.ResponseWriter, r *http.Request) {
 				im, _, err := image.Decode(nr)
 				if err != nil {
 					s.l.Info("s>", err)
-					break
-					w.WriteHeader(http.StatusInternalServerError)
-					w.Write([]byte("500"))
-					return
+					continue
 				}
-				thumbnails = append(thumbnails, resize.Resize(thumbDim, thumbDim, im, resize.NearestNeighbor))
-			case "text/json":
-				r := TRequest_{}
-				json.NewDecoder(nr).Decode(&r)
-				for _,u := range r.Urls {
-					s.l.Info("s>", u)
+				resp.addThumb(im)
 
-					resp, err := http.Get(u)
-					if err == nil {
-						im, _, err := image.Decode(resp.Body)
-						if err == nil {
-							thumbnails = append(thumbnails, resize.Resize(thumbDim, thumbDim, im, resize.NearestNeighbor))
-						}
-					}
-				}
+			case "text/json":
+				req, _ := ReadTRequest(nr)
+				req.handleUrls(s, resp)
 			}
 			nr.Close()
 		}
-
-		resp := make(TResponse, 0)
-		w.Header().Set("Content-Type", "application/json")
-		for _, v := range thumbnails {
-			var buff bytes.Buffer
-			png.Encode(&buff, v)
-			encoded := base64.StdEncoding.EncodeToString(buff.Bytes())
-			resp = append(resp, encoded)
-		}
-		encoder := json.NewEncoder(w)
-		encoder.Encode(resp)
+		resp.writeResp(w)
 	}
 
 	if ok && strings.HasPrefix(hh[0], "application/json") {
-		req := make(TRequest, 0)
-		decoder := json.NewDecoder(r.Body)
-		err := decoder.Decode(&req)
+		resp := NewTResponse()
+		req, err := ReadTRequest(r.Body)
 		if err != nil {
-			s.l.Info("s>", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("500"))
-			s.l.Info(err)
-			return
+			panic(err)
 		}
+		req.handleImgs(s, resp)
+		req.handleUrls(s, resp)
 
-		var thumbnails []image.Image
-		for _, v := range req {
-			input := base64.NewDecoder(base64.StdEncoding, strings.NewReader(v))
-			im, imageType, err := image.Decode(input)
-			if err != nil {
-				s.l.Info("s> image.Decode", err)
-
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte("500"))
-				return
-			}
-			s.l.Info("s> got", imageType)
-			thumbnails = append(thumbnails, resize.Resize(thumbDim, thumbDim, im, resize.NearestNeighbor))
-		}
-
-		resp := make(TResponse, 0)
-		w.Header().Set("Content-Type", "application/json")
-		for _, v := range thumbnails {
-			var buff bytes.Buffer
-			png.Encode(&buff, v)
-			encoded := base64.StdEncoding.EncodeToString(buff.Bytes())
-			resp = append(resp, encoded)
-		}
-		encoder := json.NewEncoder(w)
-		encoder.Encode(resp)
+		resp.writeResp(w)
 	}
 }
 
--- cmd/demo/main_test.go
+++ cmd/demo/main_test.go
@@ -5,34 +5,36 @@ import (
 	"fmt"
 	"bytes"
 	"io/ioutil"
-	"log"
 	"os"
 	"image"
 	"mime/multipart"
 	"io"
 	"net/http"
-	"bufio"
 	"encoding/base64"
 	"encoding/json"
 	"path"
 	"net/textproto"
+	"runtime/debug"
+	"bufio"
 )
 
 func init() {
-	fmt.Println("Test init()")
 	go InitServer()
 }
 
-
 const serviceUrl = "http://localhost:8080/upload"
 
+var imgsUrls = []string {
+	"https://www.google.ru/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+	"https://www.google.ru/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png",
+}
 
-func ReadFilesInDir(handler func(fileName string, imageFile *os.File)) (countImgs int) {
+func findImagesInDir(handler func(fileName string, imageFile *os.File)) (countImgs int) {
 	testDir := "../../test_data/"
 	countImgs = 0
 	files, err := ioutil.ReadDir(testDir)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	for _, f := range files {
 		imageFile, err := os.Open(testDir + f.Name())
@@ -45,7 +47,6 @@ func ReadFilesInDir(handler func(fileName string, imageFile *os.File)) (countImg
 			//fmt.Println(err)
 			continue
 		} else {
-			//fmt.Println(testDir + f.Name(), imageType)
 			countImgs++
 			imageFile.Seek(0,0)
 			handler(testDir + f.Name(), imageFile)
@@ -59,19 +60,18 @@ func handleResp(req *http.Request, countImgs int, t *testing.T) (err error){
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Print(err)
-		t.Fail()
-		return
+		panic(err)
 	}
-
 	//fmt.Println("response Status:", resp.Status)
 	//fmt.Println("response Headers:", resp.Header)
-	obj := make(TResponse, 0)
+
+	obj := TResponse{}
 	err = json.NewDecoder(resp.Body).Decode(&obj)
 	if err != nil {
-		return err
+		panic(err)
 	}
-	for _,v := range obj {
+
+	for _,v := range obj.Thumbs {
 		b, err := base64.StdEncoding.DecodeString(v)
 		if err != nil {
 			continue
@@ -90,81 +90,72 @@ func handleResp(req *http.Request, countImgs int, t *testing.T) (err error){
 	return
 }
 
+func handlePanic(t *testing.T) {
+	if r := recover(); r != nil {
+		fmt.Printf("%s: %s", r, debug.Stack()) // line 20
+		t.Fail()
+	}
+}
+
 func Test_Multipart(t *testing.T) {
+	defer handlePanic(t)
+
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
 
 	handler := func (fileName string, imageFile *os.File) {
 		part, err := writer.CreateFormFile("file", path.Base(fileName))
 		if err != nil {
-			fmt.Println(err)
-			t.Fail()
-			return
+			panic(err)
 		}
 		_, err = io.Copy(part, imageFile)
 		if err != nil {
-			fmt.Print(err)
-			t.Fail()
-			return
+			panic(err)
 		}
 	}
-	countImgs := ReadFilesInDir(handler)
-
-	jsonData := TRequest_{Urls: []string{"https://www.google.ru/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"}}
-	countImgs += len(jsonData.Urls)
+	countImgs := findImagesInDir(handler)
+	jsonData := TRequest{}
+	jsonData.Urls = imgsUrls
+	countImgs += len(imgsUrls)
 
 	hdr := make(textproto.MIMEHeader)
 	hdr.Set("Content-Type", "text/json")
-	hdr.Set("Content-Disposition", "form-data; name=\"urls\"")
+	hdr.Set("Content-Disposition", "form-data; name=\"imgsUrls\"")
 	fileWriter,_ := writer.CreatePart(hdr)
 	json.NewEncoder(fileWriter).Encode(jsonData)
-
 	err := writer.Close()
 	if err != nil {
-		fmt.Print(err)
-		t.Fail()
-		return
+		panic(err)
 	}
 
-	req, err := http.NewRequest("POST", serviceUrl, body)
+	req, _ := http.NewRequest("POST", serviceUrl, body)
 	req.Header.Set("Content-Type", writer.FormDataContentType())
-
-	err = handleResp(req, countImgs, t)
-	if err != nil {
-		fmt.Print(err)
-		t.Fail()
-	}
+	handleResp(req, countImgs, t)
 }
 
+func readFile(file *os.File) []byte {
+	fInfo, _ := file.Stat()
+	buf := make([]byte, fInfo.Size())
+	fReader := bufio.NewReader(file)
+	fReader.Read(buf)
+	return buf
+}
 
 func Test_Json(t *testing.T) {
-	images := make([]string, 0)
+	defer handlePanic(t)
+
+	jsonData := TRequest{}
 	handler := func (fileName string, imageFile *os.File) {
-		imgFile, err := os.Open(fileName)
-		if err != nil {
-			fmt.Print(">", err)
-			t.Fail()
-			return
-		}
-		fInfo, _ := imgFile.Stat()
-		buf := make([]byte, fInfo.Size())
-
-		fReader := bufio.NewReader(imgFile)
-		fReader.Read(buf)
-
-		dat := base64.StdEncoding.EncodeToString(buf)
-		images = append(images, dat)
+		imgBytes := readFile(imageFile)
+		str := base64.StdEncoding.EncodeToString(imgBytes)
+		jsonData.Imgs = append(jsonData.Imgs, str)
 	}
-	countImgs := ReadFilesInDir(handler)
-	fmt.Println(countImgs)
+	countImgs := findImagesInDir(handler)
+	jsonData.Urls = imgsUrls
+	countImgs += len(imgsUrls)
+	b, _ := json.Marshal(jsonData)
 
-	b, _ := json.Marshal(images)
-	req, err := http.NewRequest("POST", serviceUrl, bytes.NewReader(b))
+	req, _ := http.NewRequest("POST", serviceUrl, bytes.NewReader(b))
 	req.Header.Set("Content-Type", "application/json")
-
-	err = handleResp(req, countImgs, t)
-	if err != nil {
-		fmt.Print(err)
-		t.Fail()
-	}
+	handleResp(req, countImgs, t)
 }

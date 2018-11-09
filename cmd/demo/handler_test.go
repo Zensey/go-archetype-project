23b32295d28886f5cfab9079b5000f934a616487
Zensey <jnashicq@gmail.com>
2018-11-09 16:32:33 +0100

    wip

--- cmd/demo/handler_test.go
+++ cmd/demo/handler_test.go
@@ -13,8 +13,8 @@ import (
 )
 
 const (
-	nWorker   = 4
-	nRequests = 1000
+	nWorker   = 8
+	nRequests = 100
 	url       = "http://localhost:8080/"
 )
 
@@ -46,11 +46,17 @@ func Test_DiscardOld(t *testing.T) {
 type reqResult struct {
 	val int64
 	err error
+
+	time     int64
+	number   int
+	clientID int
 }
 
-func request(res chan reqResult, client *http.Client, l logger.Logger, requests int) {
+func request(res chan reqResult, client *http.Client, l logger.Logger, requests int, clientID int) {
 	for i := 0; i < requests; i++ {
-		resp, err := client.Get(url)
+		tBegin := nowNano()
+
+		resp, err := client.Get(url + "?ip=1.1.1." + strconv.Itoa(clientID))
 		if err != nil {
 			res <- reqResult{err: err}
 			return
@@ -61,20 +67,28 @@ func request(res chan reqResult, client *http.Client, l logger.Logger, requests 
 			return
 		}
 		val, err := strconv.ParseInt(string(b), 10, 64)
-		res <- reqResult{val, err}
+		delta := nowNano() - tBegin
+		res <- reqResult{val, err, delta, i + 1, clientID}
 	}
 }
 
 func runRequestsTest(ch chan reqResult, client *http.Client, l logger.Logger, t *testing.T) int64 {
 	for n := 0; n < nWorker; n++ {
-		go request(ch, client, l, nRequests)
+		go request(ch, client, l, nRequests, n)
 	}
 	maxI := int64(0)
 	for i := 0; i < nRequests*nWorker; i++ {
 		r := <-ch
+
 		assert.Nil(t, r.err)
 		if maxI < r.val {
 			maxI = r.val
+		}
+
+		// detection of a delay caused by a throttle
+		accuracy := 0.99
+		if r.time > int64(throttlePeriodSec*nanoSecMultiplier*accuracy) {
+			l.Infof("reqest-result > worker %d r# %d %f", r.clientID, r.number, float64(r.time)/float64(nanoSecMultiplier))
 		}
 	}
 	return maxI
@@ -92,10 +106,12 @@ func Test_Server(t *testing.T) {
 	err = app.saveState() // clear state
 	assert.Nil(t, err)
 
-	// run requests 3 times with 5 second pause
-	for i := 1; i <= 3; i++ {
+	// run requests 1 times with 5 second pause
+	for i := 1; i <= 1; i++ {
 		err = app.start()
 		assert.Nil(t, err)
+
+		// throttling actually breaks this test
 		maxI := runRequestsTest(ch, client, lg, t)
 		assert.Equal(t, int64(nRequests*nWorker*i)-1, maxI)
 

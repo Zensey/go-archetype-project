e32c2302137d27294fa32f4dec4a06e96ea2d347
Zensey <jnashicq@gmail.com>
2020-10-10 21:11:09 +0300

    ~

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -10,7 +10,32 @@ import (
 
 var version string
 
+type A struct {
+	t     int64
+	state int
+}
+type M struct {
+	t [1024 * 1024 * 8]int32
+	m [1024 * 1024 * 8]byte
+}
+
+func statemachine() {
+	m := M{}
+	for i := 0; i < 1024*1024*8; i++ {
+		m.m[int64(i)] = ^m.m[int64(i)]
+	}
+
+	//m := make(map[int64]A, 0)
+	//for i := 0; i < 10000*1000; i++ {
+	//	m[int64(i)] = A{}
+	//}
+
+	time.Sleep(time.Minute)
+}
+
 func main() {
+	statemachine()
+
 	l := slog.ConsoleLogger()
 	l.SetLevel(slog.LevelTrace)
 	l.Infof("Hello, World ! Version: %s", version)
